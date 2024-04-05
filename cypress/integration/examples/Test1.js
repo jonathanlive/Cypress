2e3e95075c26f5e8ff4a675490d17fa157aa5917
jonathan <jonathanlive@outlook.com>
2024-04-05 19:10:00 -0300

    Writing my first test in Cypress

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,5 @@
+describe('My First Test Suite', function(){
+    it('First Test Case that does not do much', function(){
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+    })
+})
